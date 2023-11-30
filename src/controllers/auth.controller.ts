c191a06cd635f8ed68139ff2f8ea498ee6b96600
Samuel Owadayo <odevservices@gmail.com>
2023-11-30 04:56:18 +0100

    prettier updates and bug fixes

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,43 +3,43 @@ import { Container } from 'typedi';
 import { RequestWithUser } from '@interfaces/auth.interface';
 import { User } from '@interfaces/users.interface';
 import { AuthService } from '@services/auth.service';
-import { OK } from "http-status";
+import { OK } from 'http-status';
 
 export class AuthController {
-    public auth = Container.get(AuthService);
+  public auth = Container.get(AuthService);
 
-    public signUp = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-        try {
-            const userData: User = req.body;
-            const signUpUserData: User = await this.auth.signup(userData);
+  public signUp = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const userData: User = req.body;
+      const signUpUserData: User = await this.auth.signup(userData);
 
-            res.status(OK).json({ data: signUpUserData, message: 'signup' });
-        } catch (error) {
-            next(error);
-        }
-    };
+      res.status(OK).json({ data: signUpUserData, message: 'signup' });
+    } catch (error) {
+      next(error);
+    }
+  };
 
-    public logIn = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-        try {
-            const userData: User = req.body;
-            const { cookie, findUser } = await this.auth.login(userData);
+  public logIn = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const userData: User = req.body;
+      const { cookie, findUser } = await this.auth.login(userData);
 
-            res.setHeader('Set-Cookie', [cookie]);
-            res.status(OK).json({ data: findUser, message: 'login' });
-        } catch (error) {
-            next(error);
-        }
-    };
+      res.setHeader('Set-Cookie', [cookie]);
+      res.status(OK).json({ data: findUser, message: 'login' });
+    } catch (error) {
+      next(error);
+    }
+  };
 
-    public logOut = async (req: RequestWithUser, res: Response, next: NextFunction): Promise<void> => {
-        try {
-            const userData: User = req.user;
-            const logOutUserData: User = await this.auth.logout(userData);
+  public logOut = async (req: RequestWithUser, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const userData: User = req.user;
+      const logOutUserData: User = await this.auth.logout(userData);
 
-            res.setHeader('Set-Cookie', ['Authorization=; Max-age=0']);
-            res.status(OK).json({ data: logOutUserData, message: 'logout' });
-        } catch (error) {
-            next(error);
-        }
-    };
+      res.setHeader('Set-Cookie', ['Authorization=; Max-age=0']);
+      res.status(OK).json({ data: logOutUserData, message: 'logout' });
+    } catch (error) {
+      next(error);
+    }
+  };
 }
